5c491d8aac4208d3e7632dd160d814d65ff1407f
Edi Begovic <edibegovic@gmail.com>
2019-12-12 18:40:58 +0100

    Comments changed :)

--- ml_zalando_project/algorithms/MLP/MLP.py
+++ ml_zalando_project/algorithms/MLP/MLP.py
@@ -23,27 +23,34 @@ y_test = np.array([x for x in labels_lda_test])
 
 num_classes = 5
 num_features = 4 
-layers = [200, 100, 5] #Number of nodes in each layer.
+
+#Number of nodes in each layer.
+layers = [200, 100, 5] 
 
 learning_rate = 0.001
-training_steps = 2500 # 100 Epochs
+
+# 100 Epochs
+training_steps = 2500 
 batch_size = 400
 
 ###########################################################################################
 
-
-def training_data(x_train, y_train, batch_size = 250, shuffles = 10000): #Shuffle and divide data up into training batches.
+# Shuffle and divide data up into training batches.
+def training_data(x_train, y_train, batch_size = 250, shuffles = 10000): 
     
     train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))  
-    train_data = train_data.repeat().shuffle(shuffles).batch(batch_size) #Shuffle and batch data.
+    # Shuffle and batch data.
+    train_data = train_data.repeat().shuffle(shuffles).batch(batch_size) 
     
     return train_data
 
-def init_weights(features, hiddens): #Initialize Tensors for the weights and biases of each layer.
+# Initialize Tensors for the weights and biases of each layer.
+def init_weights(features, hiddens): 
     
     weights = {}
     biases = {}
-    random_normal = tf.initializers.RandomNormal() #Initialize weights 
+    # Initialize weights 
+    random_normal = tf.initializers.RandomNormal() 
     
     for idx, neurons in enumerate(hiddens):
         if idx == 0:
@@ -55,25 +62,32 @@ def init_weights(features, hiddens): #Initialize Tensors for the weights and bia
             
     return (weights, biases)
 
-def model(images): #Formula for the fucntion of the neurons.
+# Formula for the fucntion of the neurons.
+def model(images): 
     
     for idx in range(len(weights)):
         images = tf.add(tf.matmul(images, weights[idx]), biases[idx])
         if idx != len(weights)-1:
-            images = tf.nn.relu(images) #ReLU activation fucntion.
+            #ReLU activation fucntion
+            images = tf.nn.relu(images) 
             
-    return tf.nn.softmax(images)    #This runs on the output layer to return a probability.
+    # This runs on the output layer to return a probability.
+    return tf.nn.softmax(images)    
 
-def loss_function(predictions, true):  #Multi-class Cross-entropy loss function.
+# Multi-class Cross-entropy loss function.
+def loss_function(predictions, true):  
     
     true = tf.one_hot(true, depth = num_classes)
-    predictions = tf.clip_by_value(predictions, 1e-9, 1.) #Clip values to an interval to avoid log problems.
+    #Clip values to an interval to avoid log problems.
+    predictions = tf.clip_by_value(predictions, 1e-9, 1.) 
     
     return tf.reduce_mean(-tf.reduce_sum(true * tf.math.log(predictions)))
 
-def optimization(images, labels): #Optimization of the weights and baises.
+# Optimization of the weights and baises.
+def optimization(images, labels): 
     
-    with tf.GradientTape() as tape: #GradientTape keeps a record of previous gradients in order to calculate the new ones.
+    # GradientTape keeps a record of previous gradients in order to calculate the new ones.
+    with tf.GradientTape() as tape: 
         predictions = model(images)
         loss = loss_function(predictions, labels)
         
@@ -85,7 +99,8 @@ train_data = training_data(x_train, y_train, batch_size)
 optimizer = tf.optimizers.Adam(learning_rate)
 weights, biases = init_weights(num_features, layers)
 
-for step, (x_batch, y_batch) in enumerate(train_data.take(training_steps), 1): #Run the functions to train the model.
+# Run the functions to train the model
+for step, (x_batch, y_batch) in enumerate(train_data.take(training_steps), 1): 
     
     optimization(x_batch, y_batch)
     prediction = model(x_batch)
